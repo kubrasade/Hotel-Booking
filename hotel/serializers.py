285aa0adf53d477a57e83774dfde59bdaf0fcd56
kubrasade <kubra.sade2@icloud.com>
2025-03-27 14:33:54 +0300

    Add: RoomSerializer

--- hotel/serializers.py
+++ hotel/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Hotel
+from .models import Hotel, Room
 
 class HotelSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,6 +10,26 @@ class HotelSerializer(serializers.ModelSerializer):
             "address",
             "city",
             "description",
+            "created_at",
+            "updated_at",
+        ]
+        read_only_fields = ("id", "created_at", "updated_at")
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    hotel_name = serializers.CharField(source="hotel.name", read_only=True)
+
+    class Meta:
+        model = Room
+        fields = [
+            "id",
+            "hotel",  
+            "hotel_name",  
+            "name",
+            "room_type",
+            "price",
+            "capacity",
+            "is_available",
             "created_at",
             "updated_at",
         ]
