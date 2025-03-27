6b2b6dec147fd48ffc4f6d3cb3f22cb33c4c5faa
kubrasade <kubra.sade2@icloud.com>
2025-03-27 14:36:07 +0300

    Feat(Room): Add list, retrieve and create API views for Room model

--- hotel/views.py
+++ hotel/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
-from .models import Hotel
-from .serializers import HotelSerializer
+from .models import Hotel, Room
+from .serializers import HotelSerializer,RoomSerializer
 
 class HotelDetailAPIView(generics.RetrieveAPIView):
     queryset = Hotel.objects.all()
@@ -8,3 +8,21 @@ class HotelDetailAPIView(generics.RetrieveAPIView):
 
     def get_object(self):
         return Hotel.objects.first()
+    
+class RoomListAPIView(generics.ListAPIView):
+    queryset = Room.objects.filter(is_deleted=False).order_by("id")
+    serializer_class = RoomSerializer
+
+
+class RoomDetailAPIView(generics.RetrieveAPIView):
+    queryset = Room.objects.filter(is_deleted=False)
+    serializer_class = RoomSerializer
+
+
+class RoomCreateAPIView(generics.CreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+    def perform_create(self, serializer):
+        hotel = Hotel.objects.first()
+        serializer.save(hotel=hotel)
