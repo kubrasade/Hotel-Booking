c342624b300a71d0f4d68e02f23d4612139588d3
kubrasade <kubra.sade2@icloud.com>
2025-03-27 14:37:33 +0300

    Feat: Add URL patterns for  Room API views

--- hotel/urls.py
+++ hotel/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import HotelDetailAPIView
+from .views import (
+    HotelDetailAPIView,
+    RoomListAPIView,
+    RoomDetailAPIView,
+    RoomCreateAPIView,
+)
 
 urlpatterns = [
     path("", HotelDetailAPIView.as_view(), name="hotel-detail"),
-]
+    path("rooms/", RoomListAPIView.as_view(), name="room-list"),
+    path("rooms/<int:pk>/", RoomDetailAPIView.as_view(), name="room-detail"),
+    path("rooms/create/", RoomCreateAPIView.as_view(), name="room-create"),
+    ]
