7f80aa83494547f5bdf88c2638b639e487f5c6a7
kubrasade <kubra.sade2@icloud.com>
2025-03-28 14:19:22 +0300

    Add: ReservationSerializer

--- hotel/serializers.py
+++ hotel/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Hotel, Room
+from .models import Hotel, Room, Reservation
+from .enums import ReservationStatus
 
 class HotelSerializer(serializers.ModelSerializer):
     class Meta:
@@ -34,3 +35,38 @@ class RoomSerializer(serializers.ModelSerializer):
             "updated_at",
         ]
         read_only_fields = ("id", "created_at", "updated_at")
+
+class ReservationSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    room_name = serializers.CharField(source="room.name", read_only=True)
+    hotel_name = serializers.CharField(source="room.hotel.name", read_only=True)
+
+    class Meta:
+        model = Reservation
+        fields = [
+            "id",
+            "user",
+            "room",
+            "room_name",
+            "hotel_name",
+            "check_in",
+            "check_out",
+            "total_price",
+            "status",
+            "created_at",
+            "updated_at",
+        ]
+        read_only_fields = ("id", "created_at", "updated_at", "total_price", "status")
+
+    def validate(self, data):
+        if data["check_in"] >= data["check_out"]:
+            raise serializers.ValidationError("Your check-out date must be after your check-in date.")
+        return data
+
+    def create(self, validated_data):
+        nights = (validated_data["check_out"] - validated_data["check_in"]).days
+        room = validated_data["room"]
+        total_price = room.price * nights
+        validated_data["total_price"] = total_price
+        validated_data["status"] = ReservationStatus.PENDING
+        return super().create(validated_data)
