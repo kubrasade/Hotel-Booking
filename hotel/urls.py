44b04091fca9e794e5075f6db6b72530d2991c32
kubrasade <kubra.sade2@icloud.com>
2025-03-28 14:22:09 +0300

    Feat: Add URL patterns for Reservation API views

--- hotel/urls.py
+++ hotel/urls.py
@@ -4,6 +4,9 @@ from .views import (
     RoomListAPIView,
     RoomDetailAPIView,
     RoomCreateAPIView,
+    ReservationListAPIView,
+    ReservationDetailAPIView,
+    ReservationCreateAPIView
 )
 
 urlpatterns = [
@@ -11,4 +14,7 @@ urlpatterns = [
     path("rooms/", RoomListAPIView.as_view(), name="room-list"),
     path("rooms/<int:pk>/", RoomDetailAPIView.as_view(), name="room-detail"),
     path("rooms/create/", RoomCreateAPIView.as_view(), name="room-create"),
+    path("reservations/", ReservationListAPIView.as_view(), name="reservation-list"),
+    path("reservations/<int:pk>/", ReservationDetailAPIView.as_view(), name="reservation-detail"),
+    path("reservations/create/", ReservationCreateAPIView.as_view(), name="reservation-create"),
     ]
